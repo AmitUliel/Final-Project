88d2dcd10aab8b9a3bf7d819f6242ad7cebf518e
amit1 <amit1@Amit-PC.lan>
2024-03-03 14:54:34 +0200

    commit 1

--- src/test/java/Selleniun_start_projects/Selleniun_start_projects/root_calculator.java
+++ src/test/java/Selleniun_start_projects/Selleniun_start_projects/root_calculator.java
@@ -9,26 +9,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class root_calculator {
-	 static WebDriver  browser;
-	
+	static WebDriver browser;
+
 	@Before
-	  public void setup() {
-		 
-			  System.setProperty("webdriver.gecko.driver","C:\\geckodriver-v0.34.0-win32\\geckodriver.exe");
-		      browser = new FirefoxDriver();
-		  	  browser.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
-			  String url="https://www.mathportal.org/calculators/polynomials-solvers/polynomial-roots-calculator.php";
-		      browser.get(url);
-		
-	  }
-	
-	 @Test
-		public void Tests1() {
-		 browser.findElement(By.id("val1")).sendKeys("x^2+4x+3");
-		 browser.findElement(By.id("submitButton")).click();
-		 String result= browser.findElements(By.cssSelector("p.dummy_class:nth-child(4)")).toString();
-		 System.out.print(result);
-	 }
-	    	
+	public void setup() 
+	{
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\amit1\\OneDrive\\Documents\\Study\\שנה ג\\אבטחת איכות תוכנה\\geckodriver");
+		browser = new FirefoxDriver();
+		browser.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+		String url = "https://www.mathportal.org/calculators/polynomials-solvers/polynomial-roots-calculator.php";
+		browser.get(url);
+	}
+
+	@Test
+	public void Tests1() 
+	{
+		browser.findElement(By.id("val1")).sendKeys("x^2+4x+3");
+		browser.findElement(By.id("submitButton")).click();
+		String result = browser.findElements(By.cssSelector("p.dummy_class:nth-child(4)")).toString();
+		System.out.print(result);
+	}
 
 }
